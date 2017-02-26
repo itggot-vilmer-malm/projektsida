b176efbc5a5c2065b01b60cefa6e14a616b142c8
itggot-vilmer-malm <vilmer.malm@itggot.se>
2017-02-26 20:18:05 +0100

    klar
    klar för inlämning

---
+++ site/js/main.js
@@ -0,0 +1,95 @@
+
+function hideMenu() {
+    menu = document.querySelector(".senap");
+    menu.classList.remove('visible');
+    menu.classList.add('hidden');
+}
+
+function showMenu() {
+    menu = document.querySelector(".senap");
+    menu.classList.remove('hidden');
+    menu.classList.add('visible')
+}
+
+function dropMenu1(){
+	if(document.getElementById("subMenu1").className == "hidden")
+	document.getElementById("subMenu1").className = "visible";
+	else
+	document.getElementById("subMenu1").className = "hidden";
+}
+
+function dropMenu2(){
+	if(document.getElementById("subMenu2").className == "hidden")
+	document.getElementById("subMenu2").className = "visible";
+	else
+	document.getElementById("subMenu2").className = "hidden";
+}
+
+
+(function() {
+	
+	function Slideshow( element ) {
+		this.el = document.querySelector( element );
+		this.init();
+	}
+	
+	Slideshow.prototype = {
+		init: function() {
+			this.wrapper = this.el.querySelector( ".slider-wrapper" );
+			this.slides = this.el.querySelectorAll( ".slide" );
+			this.previous = this.el.querySelector( ".slider-previous" );
+			this.next = this.el.querySelector( ".slider-next" );
+			this.index = 0;
+			this.total = this.slides.length;
+			this.timer = null;
+			
+			this.action();
+			this.stopStart();	
+		},
+		_slideTo: function( slide ) {
+			var currentSlide = this.slides[slide];
+			currentSlide.style.opacity = 1;
+			
+			for( var i = 0; i < this.slides.length; i++ ) {
+				var slide = this.slides[i];
+				if( slide !== currentSlide ) {
+					slide.style.opacity = 0;
+				}
+			}
+		},
+		action: function() {
+			var self = this;
+			self.timer = setInterval(function() {
+				self.index++;
+				if( self.index == self.slides.length ) {
+					self.index = 0;
+				}
+				self._slideTo( self.index );
+				
+			}, 3000);
+		},
+		stopStart: function() {
+			var self = this;
+			self.el.addEventListener( "mouseover", function() {
+				clearInterval( self.timer );
+				self.timer = null;
+				
+			}, false);
+			self.el.addEventListener( "mouseout", function() {
+				self.action();
+				
+			}, false);
+		}
+		
+		
+	};
+	
+	document.addEventListener( "DOMContentLoaded", function() {
+		
+		var slider = new Slideshow( "#main-slider" );
+		
+	});
+	
+	
+})();
+
